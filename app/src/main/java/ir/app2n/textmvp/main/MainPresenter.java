43145fd9b608c81232624f42590c7ac0b6c384c2
hojjjat ghasemabadi <hojjatgh6220@gmail.com>
2020-07-22 12:47:04 +0430

    design patern

--- app/src/main/java/ir/app2n/textmvp/main/MainPresenter.java
+++ app/src/main/java/ir/app2n/textmvp/main/MainPresenter.java
@@ -1,20 +1,22 @@
 package ir.app2n.textmvp.main;
 
 import android.app.usage.ConfigurationStats;
+import android.view.View;
 
 public class MainPresenter implements MainContract.Presenter {
-    private MainContract.MvpView  mapView;
+    private MainContract.MvpView mapView;
+
     public MainPresenter(MainContract.MvpView view) {
-        mapView=view;
+        mapView = view;
     }
 
     @Override
-    public void HandleSigninButtonClick() {
-
+    public void HandleSigninButtonClick(View view) {
+        mapView.ShowSigninScreen();
     }
 
     @Override
-    public void HandleSignOutButtonClick() {
-
+    public void HandleSignOutButtonClick(View view) {
+        mapView.ShowsignOutScreen();
     }
 }
